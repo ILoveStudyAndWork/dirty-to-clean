d43cba9af21d193e3fa44d80e6f197a738985255
ILoveStudyAndWork <fengyi_ling@qq.com>
2022-04-19 20:58:54 +0800

    [Fengyi]Add comment in UsePipelineReplaceLoops as bad comment example

--- src/main/java/loops/UsePipelineReplaceLoops.java
+++ src/main/java/loops/UsePipelineReplaceLoops.java
@@ -9,10 +9,12 @@ public class UsePipelineReplaceLoops {
 
     // Requirement find the oldest students whose name start with A
     public static void main(String[] args) {
+        // prepare student list
         List<Student> students = prepareStudents();
         int maxMge = 0;
         Student oldestStudent = null;
         for (Student student : students) {
+            // filter the student whose name start with A
             if (student.getName().startsWith("A")) {
                 if (student.getAge() > maxMge) {
                     maxMge = student.getAge();
@@ -20,14 +22,14 @@ public class UsePipelineReplaceLoops {
                 }
             }
         }
-
+        // print the information of the oldest student if present
         if (Objects.nonNull(oldestStudent)) {
-            System.out.printf("Oldest student:%s, age: %d" , oldestStudent.getName(), oldestStudent.getAge() );
+            System.out.printf("Oldest student:%s, age: %d", oldestStudent.getName(), oldestStudent.getAge());
         }
     }
 
     public static List<Student> prepareStudents() {
-        return Lists.newArrayList(new Student("Alex", 16),
+        return Lists.newArrayList(new Student("Angle", 15),
                 new Student("Alex", 16),
                 new Student("Bob", 7),
                 new Student("Alan", 14),
