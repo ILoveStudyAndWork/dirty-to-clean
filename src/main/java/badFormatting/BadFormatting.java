093065b23b6b8d7660f1854bbc5e082f7b3003d9
ILoveStudyAndWork <fengyi_ling@qq.com>
2022-04-19 20:49:45 +0800

    [Fengyi]Add bad formatting example

---
+++ src/main/java/badFormatting/BadFormatting.java
@@ -0,0 +1,9 @@
+package badFormatting;
+import java.util.List;
+public class BadFormatting {
+private String name;private int age;private String nickName;private List<String> hobby;public String basicIntroduce(){
+                            return String.format("my name is %s, I'm %d years old", getName(), getAge()) ;
+}public String getName() {return name;}
+    public int getAge() {
+return age;
+    }}
