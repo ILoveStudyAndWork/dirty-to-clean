7e455ab723f85798cb802c064f167498e92222b0
ILoveStudyAndWork <fengyi_ling@qq.com>
2022-04-21 19:48:52 +0800

    [Fengyi]Refactor:use shortcut option + cmd + l to format

--- src/main/java/bad_formatting/BadFormatting.java
+++ src/main/java/bad_formatting/BadFormatting.java
@@ -1,9 +1,22 @@
 package bad_formatting;
+
 import java.util.List;
+
 public class BadFormatting {
-private String name;private int age;private String nickName;private List<String> hobby;public String basicIntroduce(){
-                            return String.format("my name is %s, I'm %d years old", getName(), getAge()) ;
-}public String getName() {return name;}
+    private String name;
+    private int age;
+    private String nickName;
+    private List<String> hobby;
+
+    public String basicIntroduce() {
+        return String.format("my name is %s, I'm %d years old", getName(), getAge());
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public int getAge() {
-return age;
-    }}
+        return age;
+    }
+}
